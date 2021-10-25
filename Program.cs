74950955f0b10061dfc468f0dd525b4e29f1e9fb
Gabrielly Sanches <gabrielly.sanches0506@gmail.com>
2021-10-25 00:56:56 -0300

    Numeros Multiplicados_Subindo Exercicio

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, digite os 3 numeros:");
+            string numeros = Console.ReadLine();
+            var numerosArray = numeros.Split(",");
+
+            int resultado = 1;
+
+            foreach (string item in numerosArray)
+            {
+                resultado *= int.Parse(item);
+            }
+
+            Console.WriteLine("O resultado é " + resultado + ".");
+
+
+            if (resultado > 10 && resultado < 20)
+            {
+                Console.WriteLine("Sim, é verdadeiro, achou o número premiado!");
+            }
+
+            else if (resultado > 100 && resultado < 200)
+            {
+                Console.WriteLine("Sim, é verdadeiro, achou o número premiado!");
+            }
+
+            else if (resultado > 1000 && resultado < 2000)
+            {
+                Console.WriteLine("Sim, é verdadeiro, achou o número premiado!");
+            }
+
+            else
+            {
+                Console.WriteLine("Infelizmente você perdeu!");
+            }
+
+
+
+        }
+    }
+}
